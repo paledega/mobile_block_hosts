de5ad974f9c980e995b54332dbfaee778f939e97
tknr <info@tknr.com>
2018-10-06 14:54:34 +0900

    2018年 10月  6日 土曜日 14:54:34 JST

--- update.sh
+++ update.sh
@@ -2,9 +2,12 @@
 
 rm -f hosts.txt
 rm -f hosts.zip
-wget "http://www.geocities.jp/qgdjg589/hosts.zip"
+wget "http://www.geocities.jp/qgdjg589/hosts.zip" | exit 1
 unzip hosts.zip
+mv hosts/hosts.txt .
+rm -f hosts.zip
+rm -Rf hosts
 
-wget "https://sites.google.com/site/hosts2ch/ja"
+wget "https://sites.google.com/site/hosts2ch/ja" | exit 1
 cat ja >> hosts.txt
 rm -f ja
